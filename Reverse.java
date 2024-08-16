66da31b0171e9fa987aa45c80e34b462ede7b312
KANITA VERMA <vermakanita13@gmail.com>
2024-08-16 21:28:51 +0530

    Add files via upload

---
+++ Reverse.java
@@ -0,0 +1,8 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int arr[] = {3,5,7,9,8};
+        for(int i =arr.length-1;i>=0;i--){
+       System.out.print(arr[i]);
+        }
+    }
+}
