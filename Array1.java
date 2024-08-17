368487cf2d0a74248df250322f4c3d3cc31c4ba1
KANITA VERMA <vermakanita13@gmail.com>
2024-08-17 14:31:25 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,16 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int arr[]={1,35,676666,83,25};
+        int max=arr[0];
+            for(int i=0;i<arr.length;i++){
+            if(arr[i]>max)
+                max=arr[i];
+            }
+            System.out.println(max);
+        }
+    }
+    
+
+
+
+
