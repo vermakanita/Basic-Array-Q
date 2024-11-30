b56845e163aa3d94d12a4add34df06c4928e337a
KANITA VERMA <vermakanita13@gmail.com>
2024-11-30 20:31:17 +0530

    Add files via upload

---
+++ LOOP.java
@@ -0,0 +1,16 @@
+// public class LOOP 
+// {
+//     public static void main(String[] args) {
+//         int marks [5];
+
+//         for ()
+//     }
+// }
+class h{
+    public static void main(String []args){
+        int x=5;
+         int z=x++ 
+         --x;
+        System.out.println(x++,--x);
+    }
+ }
