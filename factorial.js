386659c95d2403b60f9b1f7bcb767eeef50dbc57
KANITA VERMA <vermakanita13@gmail.com>
2025-01-02 23:49:08 +0530

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,15 @@
+// 10 to 20
+
+for(let i=1;i<=6;i++){
+    let fact =1;
+    for(let j=1;j<=i;j++){
+        fact=parseInt(fact*j);
+    }
+    console.log(fact)
+}
+
+// let fact=1;
+// for(let i=1;i<=5;i++){
+//     fact=fact*i
+// }
+// console.log(fact)
