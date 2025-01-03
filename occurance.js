fd6bb15cc78e920efdab42d54634b4d596748d39
KANITA VERMA <vermakanita13@gmail.com>
2025-01-03 23:06:19 +0530

    Add files via upload

---
+++ occurance.js
@@ -0,0 +1,37 @@
+
+//find occurance 
+let a= "helloo";
+let obj = {};
+
+// for(let i=0;i<a.length;i++){
+//     let ele = a[i];
+
+//     if(!obj[ele]){
+//         obj[ele]=1;
+//     }
+//     else{
+//         obj[ele]=obj[ele]+1;
+//     }
+// }
+// console.log(obj);
+
+
+// for(let i=0;i<a.length;i++){
+//     obj={}
+    
+
+//     if(!obj[i]){
+//         obj[i]=1;
+//         console.log(i)
+
+//         console.log("if")
+        
+//     }
+//     else{
+//         console.log("else")
+
+//         obj[i]=obj[i]+1;
+        
+//     }
+// }
+// console.log(obj);
