352d6e6cd4ce38a63ed6b0113e5d9b29598c2a34
KANITA VERMA <vermakanita13@gmail.com>
2025-01-05 22:08:49 +0530

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,28 @@
+
+public class Q2 {
+
+    
+
+
+public static void main(String[] args) {
+    int n=5; 
+    for(int i=1;i<=n;i++){
+        int k=1;
+
+    
+    for(int j=1;j<=i;j++){
+        System.out.print(k);
+        k++;
+
+    }
+    k--;
+    for(int a=1;a<=i-1;a++){
+        k--;
+        System.out.print(k);
+        // k--;
+    }
+    System.out.println();
+}
+// System.out.println();
+}
+}
